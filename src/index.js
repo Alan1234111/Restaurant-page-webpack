4ba712b5f6041d24cb3361f0d3e8a4d072a01fd3
Alan Pason <alan.pason132@gmail.com>
2023-02-08 15:33:58 +0100

    add: checkMenuBtn

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ function addNavEvents() {
   const homeBtn = document.getElementById("home");
   const menuBtn = document.getElementById("menu");
   const aboutBtn = document.getElementById("about");
+  const checkMenuBtn = document.querySelector(".restaurant-menu-btn");
 
   homeBtn.addEventListener("click", loadHome);
   menuBtn.addEventListener("click", loadMenu);
   aboutBtn.addEventListener("click", loadAbout);
+  checkMenuBtn.addEventListener("click", loadMenu);
 }
 
 function init() {
